88f21a68c8f5e123551e9fc6d773c03b14db5abf
singlerider <haroboy876@gmail.com>
2015-01-31 17:25:37 -0500

    Got the form thing working. Need to hide password input

--- src/lib/commands/songrequest.py
+++ src/lib/commands/songrequest.py
@@ -5,7 +5,7 @@ Developed by Shane Engelman <me@5h4n3.com>
 ####Use this to run the songrequest script
 import subprocess
 import src.config.config as config
-import src
+import sys
 import time
 import pexpect
 
@@ -28,35 +28,13 @@ def songrequest(args):
             add_to_playlist = "add " + config.spotify_playlist + " 0 " + " ".join([artist, title]).replace('_'," ")
             
             # execute spshell:
-            p = subprocess.Popen(["/home/shane/builds/libspotify-12.1.51-Linux-x86_64-release/share/doc/libspotify/examples/spshell/spshell"], stdin=subprocess.PIPE,
-                                                         stdout=subprocess.PIPE,
-                                                         stderr=subprocess.STDOUT)
-            #Using libspotify 12.1.51.g86c92b43 Release Linux-x86_64 
-            print p.stdout.readline()
-            print p.stdout.read(len("Username (just press enter to login with stored credentials): "))
-            print "sending login info:", login
-            p.stdin.write(login+"\n")
-            # Read the newline after the login
-            print p.stdout.readline()
-            print p.stdout.read(len("Password: "))            
-            p.stdin.write(password+"\n")
-            # Read the newline after the login
-            print p.stdout.readline()
-            
-            # Read the logged in message which is two lines
-            for i in range(2):
-                print p.stdout.readline()
-            
-            p.stdin.write("search " + add_to_playlist + "\n")
-            p.stdin.close()
-            print p.stdout.read()
-            
+
             
             child = pexpect.spawn('/home/shane/builds/libspotify-12.1.51-Linux-x86_64-release/share/doc/libspotify/examples/spshell/spshell')
             child.logfile = sys.stdout
-            child.expect('Username (just press enter to login with stored credentials): ')
+            child.expect('Username')
             child.sendline(login)
-            child.expect('Password: ')
+            child.expect('Password')
             child.sendline(password)
             child.expect('Logged in to Spotify as user')
             child.sendline("search " + add_to_playlist)
