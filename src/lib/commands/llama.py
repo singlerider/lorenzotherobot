66ac8a0269ebff29f84a4605d9d37b61ef503139
singlerider <haroboy876@gmail.com>
2015-02-06 22:54:30 -0500

    Huge breakthrough. Llama command will return concatenated list of users currently in chat.

--- src/lib/commands/llama.py
+++ src/lib/commands/llama.py
@@ -2,43 +2,47 @@ import sqlite3
 import sys
 import re
 import socket
-
+import pexpect
+import time
+import os
 import src.lib.user_data as user_data
+import os
+import urllib2
+import ast
 
-def llama():
-    #allocate memory for operations
-    db = sqlite3.connect(':memory:')
+"""Database in progress. This will run as a cron job and will serve as the points counter and Pokemon assigning tool"""
+def database_entry():
     #create/connect to user database
     db = sqlite3.connect('/home/shane/git/lorenzotherobot/src/lib/userdata.db')
-
-    # Get a cursor object
+    
+    #get a cursor object
     cursor = db.cursor()
     
-#####Delete the below lines if first time running
- 
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS users(name TEXT PRIMARY KEY,
-                       pokemon TEXT, treats TEXT, ul TEXT)
-    ''')
-    
+                        pokemon TEXT, treats TEXT, ul TEXT)
+     ''')
     for p in user_data.user_data_points:
         format_str = """INSERT INTO users (name, pokemon, treats, ul)
         VALUES ("{name}", "{pokemon}", "{treats}", "{ul}" );"""
-    
-        sql_command = format_str.format(name=p[0], pokemon=p[1], treats = p[2], ul = p[3])
+        sql_command = format_str.format(name=p[0], pokemon=p[1], treats=p[2], ul=p[3])
         cursor.execute(sql_command)
-   
     cursor.execute("SELECT * FROM users")
-    
-    #prints all entries 
+    #prints all entires
     print("fetchall:")
-    result = cursor.fetchall() 
+    result = cursor.fetchall()
     for r in result:
         print(r)
-    cursor.execute("SELECT * FROM users") 
+    cursor.execute("SELECT * FROM users")
     #prints one entry
     print("\nfetch one:")
     res = cursor.fetchone()
-     
     print(res)
     db.close()
+
+"""Gets list of users and returns them to the chat"""
+
+def llama():
+    response = urllib2.urlopen('https://tmi.twitch.tv/group/user/lorenzotherobot/chatters')#change username to your channel
+    user_dict = ast.literal_eval(response.read())
+    return str("Viewers: " + ", ".join(user_dict["chatters"]["moderators"]) + ", " + ", ".join(user_dict["chatters"]["viewers"]))
