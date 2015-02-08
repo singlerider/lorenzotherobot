52b5f2946f207a73606d39ae047423e4606dcaf7
singlerider <haroboy876@gmail.com>
2015-02-08 03:50:36 -0500

    Thanks to dustinbcox for assistance on database class template

--- src/lib/commands/llama.py
+++ src/lib/commands/llama.py
@@ -14,45 +14,63 @@ response = urllib2.urlopen('https://tmi.twitch.tv/group/user/freshnica/chatters'
 user_dict = ast.literal_eval(response.read())
 
 """Database in progress. This will run as a cron job and will serve as the points counter and Pokemon assigning tool"""
-def llama():
-    #create/connect to user database
-    db = sqlite3.connect('/home/shane/git/lorenzotherobot/src/lib/userdata.db')
-    
-    moderator_list = "[" + ", ".join(user_dict["chatters"]["moderators"]) + "]"
-    user_list = ", ".join(user_dict["chatters"]["viewers"])
-    
-    combined_string = ", ".join((moderator_list, user_list))
-    combined_list = "[(" + "), (".join(user_dict["chatters"]["moderators"]) + "), (".join(user_dict["chatters"]["viewers"]) + ")]"
-    
-    print combined_list
-    
-    #get a cursor object
-    cursor = db.cursor()
-    
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users(name TEXT
-                        )
-     ''')
-
-        
-    format_str = """INSERT INTO users ("name")
-    VALUES ("{name}");"""
-    sql_command = format_str.format(name=combined_list)
-    cursor.execute(sql_command)
-    cursor.executemany('INSERT INTO users VALUES (?)', combined_list)
-    print "Inserted into database!"    
-    #cursor.execute("SELECT * FROM users")
-    #prints all entires
-    #print("fetchall:")
-    result = cursor.fetchall()
-    #for r in result:
-    #    print(r)
-
-    #prints one entry
-    #print("\nfetch one:")
-    res = cursor.fetchone()
-    #print(res)
-    db.close()
+import sqlite3
+ 
+ 
+class UserData (object):
+    """ Save the points to a database """
+ 
+    # Start off with 5 points
+    INITIAL_VALUE = 5
+ 
+    def __init__(self, filepath):
+        """ Initialize the database as needed """
+        self.filepath = filepath
+        conn = sqlite3.connect(self.filepath)
+        conn.execute("""CREATE TABLE IF NOT EXISTS users
+                    (username VARCHAR(50) PRIMARY KEY,
+                    points INTEGER);""")
+        conn.commit()
+        conn.close()
+ 
+    def save(self, users):
+        for user in users:
+            if self.get_user(user) is None:
+                conn = sqlite3.connect(self.filepath)
+                # Let's add the user then
+                conn.execute("INSERT INTO users VALUES(?,?)", (user,
+                             self.INITIAL_VALUE))
+                conn.commit()
+                conn.close()
+            else:
+                conn = sqlite3.connect(self.filepath)
+                # Let's update the existing user
+                conn.execute("UPDATE users SET points = points + ?" +
+                             " WHERE username = ?", (self.INITIAL_VALUE, user))
+                conn.commit()
+                conn.close()
+ 
+    def get_user(self, username):
+        conn = sqlite3.connect(self.filepath)
+        cursor = conn.execute("SELECT points FROM users WHERE username = ?",
+                              (username,))
+        points = cursor.fetchone()
+        if points is not None:
+            points = points[0] # get only the points from the tuple
+        conn.close()
+        return points
+ 
+ 
+if __name__ == "__main__":
+    llama = UserData("sample_sqlite_database.db")
+ 
+    my_users = ['greg', 'jerry', 'larry', 'sam', 'jenny', 'moe', 'cindy',
+                'shane']
+    llama.save(my_users)
+    print "Users:"
+    for user in my_users:
+        print "User:", user, " ",
+        print llama.get_user(user)
     
     return str("Viewers: " + ", ".join(user_dict["chatters"]["moderators"]) + ", " + ", ".join(user_dict["chatters"]["viewers"]))
     
