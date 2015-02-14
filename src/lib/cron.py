b62e55cf6766a1692dba8e50494160bfccb0aaf8
Shane Engelman <haroboy876@gmail.com>
2015-02-14 16:03:55 -0500

    Attempting to see if I can get these cron jobs to run without a needless message

--- src/lib/cron.py
+++ src/lib/cron.py
@@ -35,11 +35,11 @@ class cron:
 				# "("+ ",".join(self.args[index]) + ")"
 				pbot('[CRON] ' + self.functions[index].__name__ , self.channel)
 				
-				message = str(self.functions[index](self.args[index]))
+				message = self.functions[index](self.args[index])
 				
 				if message != None:
 				
-					self.irc.send_message(self.channel, message[:120])
+					str(self.irc.send_message(self.channel, message[:120]))
 		
 					self.last_ran = time.time()
 		
