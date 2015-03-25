d513b038551c6b06f7b7e97425831c321ab3badf
theepicsnail <theepicsnail@gmail.com>
2015-03-24 22:06:30 -0700

    Adding missing variables to globals

--- globals_example.py
+++ globals_example.py
@@ -3,3 +3,7 @@ channel = "curvyllama"
 YOUTUBE_DEVELOPER_KEY = "JKSHJKSHDKJSHjksjkfnhsdjkhfjksdhfsdjk"
 YOUTUBE_PLAYLIST = "PLNjkshdfjkshKjdfhskfshdfjkhsdfjJKJK"
 CURRENT_USER = ""
+VARIABLE = ""
+POKEMON = ""
+CAUGHT = ""
+
