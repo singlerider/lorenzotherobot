80a22fe8241dfbd110b6d3aa21714c160c828c43
theepicsnail <theepicsnail@gmail.com>
2015-03-26 13:01:17 -0700

    pokedex threading issues

--- src/lib/commands/pokedex/pokedex.py
+++ src/lib/commands/pokedex/pokedex.py
@@ -3,8 +3,11 @@ import os
 
 DATABASE_FILE = os.path.abspath(os.path.join(__file__, "../../..", "pokemon.db"))
 
+def getConnection():
+  return sqlite3.connect(DATABASE_FILE)
+
 #Create the table if it doesn't exist.
-conn = sqlite3.connect(DATABASE_FILE)
+conn = getConnection()
 conn.execute("""CREATE TABLE IF NOT EXISTS users
             (user_data_name VARCHAR(50) PRIMARY KEY,
             pokemon_value VARCHAR);""")
@@ -12,11 +15,13 @@ conn.commit()
 
 
 def setPokemon(user, pokemon):
+  conn = getConnection()
   cmd = "INSERT OR REPLACE INTO users VALUES (?,?)"
   conn.execute(cmd, (user, pokemon))
   conn.commit()
 
 def getPokemon(user):
+    conn = getConnection()
     cmd = "SELECT pokemon_value FROM users WHERE user_data_name = ?"
     cursor = conn.execute(cmd, (user,))
     conn.commit()
