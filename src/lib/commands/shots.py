1d0cbb025753c2f2aedace26d2f627aa7f723381
theepicsnail <theepicsnail@gmail.com>
2015-03-26 17:32:37 -0700

    Persistant shots

--- src/lib/commands/shots.py
+++ src/lib/commands/shots.py
@@ -4,15 +4,20 @@ Developed Shane Engelman <me@5h4n3.com>
 # IN ORDER FOR THIS TO RUN CORRECTLY
 # run '!shots init 0' the first time
 import globals
+import pickle
 
+def writeShots():
+    pickle.dump(shot_count, open("shots.pkl", "wb"))
+def readShots():
+    return pickle.load(open("shots.pkl", "rb"))
 
-shot_count = 0
+shot_count = readShots()
 usage = "!shots (add/remove/set [amount])"
 def shots(args):
     global shot_count
 
     approved_list = [
-        'curvyllama', 'peligrosocortez', 'singlerider', 'newyork_triforce']
+            'curvyllama', 'peligrosocortez', 'singlerider', 'newyork_triforce']
 
     if globals.CURRENT_USER not in approved_list:
         return "Only " + ", ".join(approved_list) + " are allowed to do that!"
@@ -31,7 +36,7 @@ def shots(args):
     elif action == "set":
       shot_count = delta
 
+    writeShots()
+
     return "Alright! Now there's {} left".format(shot_count)
-
-
 
