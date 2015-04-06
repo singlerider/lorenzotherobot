f0d685997001ab990a3593ee393ca6db6f8cb351
singlerider <haroboy876@gmail.com>
2015-04-05 23:19:00 -0400

    Re-added CAUGHT = True/False to catch and cron logic

--- src/lib/commands/catch.py
+++ src/lib/commands/catch.py
@@ -5,12 +5,16 @@ from src.lib.queries.pokemon_queries import *
 
 
 def catch():
-    pokemon_trainer = globals.CURRENT_USER
-    open_position = find_open_party_positions(pokemon_trainer)
-    desired_level = 5
-    pokemon_id = get_pokemon_id_from_name(globals.POKEMON)
-    print pokemon_id
-    if len(open_position) > 0:
-        return insert_user_pokemon(pokemon_trainer, pokemon_trainer, open_position[0], pokemon_id, desired_level, globals.POKEMON, None, None)
+    if globals.CAUGHT == False:
+        pokemon_trainer = globals.CURRENT_USER
+        open_position = find_open_party_positions(pokemon_trainer)
+        desired_level = 5
+        pokemon_id = get_pokemon_id_from_name(globals.POKEMON)
+        print pokemon_id
+        if len(open_position) > 0:
+            globals.CAUGHT = True
+            return insert_user_pokemon(pokemon_trainer, pokemon_trainer, open_position[0], pokemon_id, desired_level, globals.POKEMON, None, None)
+        else:
+            return "No open slots in your party."
     else:
-        return "No open slots in your party."
+        return "Too slow!"
