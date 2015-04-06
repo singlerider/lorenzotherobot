3d94427a321b6e24e1774effc13369e303ef8be6
singlerider <haroboy876@gmail.com>
2015-04-06 15:33:25 -0400

    Added hourly advertisement capability

--- src/config/config_example.py
+++ src/config/config_example.py
@@ -22,6 +22,7 @@ config = {
             #time, run, callback
             (120, True, pokemon.cron), # pokemon released every 2 minutes
             (300, True, treats.cron), # treat handed out every 5 minutes
+            (3600, True, advertisement.cron), # advertisement for streamer choice every hour
         ],
     },
 }
---
+++ src/lib/commands/advertisement.py
@@ -0,0 +1,5 @@
+def cron():
+    return advertisement()
+
+def advertisement():
+    return "If you like the designs, check out the designer at [placeholder]"
