65e7ef415869ec6e58e379428d0564ca3fb0f59d
singlerider <haroboy876@gmail.com>
2015-04-16 20:10:09 -0400

    Modified battle message

--- src/lib/commands/followers.py
+++ src/lib/commands/followers.py
@@ -7,4 +7,4 @@ def followers():
     stream_followers = llama_import.get_stream_followers()
     follower_list = str(stream_followers["follows"][0]["user"]["display_name"]) + ", " + str(stream_followers["follows"][1]["user"]["display_name"]) + ", " + str(stream_followers[
         "follows"][2]["user"]["display_name"]) + ", " + str(stream_followers["follows"][3]["user"]["display_name"]) + ", " + str(stream_followers["follows"][4]["user"]["display_name"])
-    return "In case you missed them, here are the five most recent Llamas: " + follower_list
+    return globals.channel + "'s most recent followers: " + follower_list
