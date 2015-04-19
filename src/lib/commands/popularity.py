f958d47d280d8770b847b5a0ffbce7db345976bb
singlerider <haroboy876@gmail.com>
2015-04-19 12:11:52 -0400

    More minor bug fixes

--- src/lib/commands/popularity.py
+++ src/lib/commands/popularity.py
@@ -4,13 +4,13 @@ def popularity(args):
     
     game = args[0]
     
-    if "fresh" or "freshnica" == game.lower():
+    if game.lower() == "fresh" or "freshnica":
         return "Who is freshnica, culo?"
-    elif "singlerider" or "shane" == game.lower():
+    elif game.lower() == "singlerider" or "shane":
         return "10 out of 10 B)"
-    elif "newyork" or "triforce" == game.lower():
+    elif game.lower() == "newyork" or "triforce":
         return "1 out of ResidentSleeper"
-    elif "curvy" or "amanda" == game.lower():
+    elif game.lower() == "curvy" or "amanda":
         return "Amanda sits just outside of the crazy zone (hovering at a 7.9), but far into the hot zone (10 out of 10), making her an actual unicorn. Catch her now!"
     
     else:
