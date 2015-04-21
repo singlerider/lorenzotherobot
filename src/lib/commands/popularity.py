2c8e79b4c9c2f17573c21d5055b1546f0fd0e83a
Shane Engelman <haroboy876@gmail.com>
2015-04-21 05:18:49 -0400

    Got rid of Easter Egg

--- src/lib/commands/popularity.py
+++ src/lib/commands/popularity.py
@@ -4,14 +4,6 @@ def popularity(args):
     
     game = args[0]
     
-    if "fresh" in game.lower():
-        return "Who is " + game.lower() + ", culo?"
-    elif "singlerider" in game.lower() or "shane" in game.lower():
-        return "10 out of 10 B)"
-    elif "newyork" in game.lower() or "triforce" in game.lower():
-        return "1 out of ResidentSleeper"
-    elif "curvy" in game.lower() or "amanda" in game.lower():
-        return "Amanda sits just outside of the crazy zone (hovering at a 7.9), but far into the hot zone (10 out of 10), making her an actual unicorn. Catch her now!"
     
-    else:
-        return get_game_popularity(game)
+
+    return get_game_popularity(game)
