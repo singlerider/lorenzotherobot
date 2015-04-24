7c96eea7319c3b6c0e237dce2e3ca05abdee7cac
singlerider <haroboy876@gmail.com>
2015-04-24 03:10:39 -0400

    Added safety checks to !trade

--- src/lib/commands/trade.py
+++ src/lib/commands/trade.py
@@ -19,12 +19,22 @@ def trade(args):
     asking_level = args[2]
     #trade_transaction(giver, giver_position, receiver, receiver_position)
     try:
-        asking_pokemon_id = get_pokemon_id_from_name(pokemon_to_trade)
-        if asking_pokemon_id != "Error":
-            set_pokemon_trade_status(trade_set_time, asking_pokemon_id, asking_level, globals.CURRENT_USER, party_position)
-            return "Success. Your Pokemon has been set to 'tradable' for the next 24 hours. Use !check [username] to view your listing!"
-        else:
-            return "spelling error"
+        try:
+            if int(args[0]) and int(args[2]):
+                if int(args[2]) <= 100:
+                    if int(args[2]) >= 5:
+                        asking_pokemon_id = get_pokemon_id_from_name(pokemon_to_trade)
+                        if asking_pokemon_id != "Error":
+                            set_pokemon_trade_status(trade_set_time, asking_pokemon_id, asking_level, globals.CURRENT_USER, party_position)
+                            return "Success. Your Pokemon has been set to 'tradable' for the next 24 hours. Use !check [username] to view your listing!"
+                        else:
+                            return "spelling error"
+                    else:
+                        return "The requested level must be at least 5."
+                else:
+                    return "The requested level must be lower than 100."
+        except:
+            return "Position and requested level must be numbers!"
     except Exception, error:
         return "FailFish: " + str(error)
             
