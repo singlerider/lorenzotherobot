727f3bb2e88be850f8879c5244a37558d9480404
singlerider <haroboy876@gmail.com>
2015-04-30 02:54:03 -0400

    Added gifting of items

--- src/lib/commands/gift.py
+++ src/lib/commands/gift.py
@@ -1,7 +1,7 @@
 from src.lib.queries.pokemon_queries import *
 from src.lib.queries.points_queries import *
 
-usage = '!gift [username] [pokemon_name] [pokemon_nickname] [level]'
+usage = '!gift [username] [pokemon_name] [level]'
 
 def gift(args):
     
@@ -9,23 +9,27 @@ def gift(args):
     open_position, occupied_positions = find_open_party_positions(username)
     name = args[1]
     level = abs(int(args[2]))
-     
     
     try:
         get_user_points(username)
-        if len(open_position) > 0:
-            if level <= 100:
-                try:
-                    get_pokemon_id_from_name(name)
-                    id_from_name = get_pokemon_id_from_name(name)
-                    globals.CAUGHT = True
-                    return insert_user_pokemon(username, username, open_position[0], id_from_name, level, id_from_name, None, None)
-                except:
-                    return "Check your spelling and capitalization!"
-            else:
-                return "There is a level cap of 100"
+        
+        if name == "item":
+            id = args[2]
+            return buy_items(id, username)
         else:
-            return "No open slots in their party." 
+            if len(open_position) > 0:
+                if level <= 100:
+                    try:
+                        get_pokemon_id_from_name(name)
+                        id_from_name = get_pokemon_id_from_name(name)
+                        globals.CAUGHT = True
+                        return insert_user_pokemon(username, username, open_position[0], id_from_name, level, id_from_name, None, None)
+                    except:
+                        return "Check your spelling and capitalization!"
+                else:
+                    return "There is a level cap of 100"
+            else:
+                return "No open slots in their party." 
     except:
         return "Are you sure that user exists?"
     
