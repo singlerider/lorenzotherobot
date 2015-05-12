0f59c1f234bbd41d24042d8e7e4911ee896f9b26
Shane Engelman <haroboy876@gmail.com>
2015-05-12 03:05:36 -0400

    adding urban.py initial - not tested

---
+++ src/lib/commands/urban.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def urban(args):
+    word = args[0].replace(' ','%20')
+    word_url = 'http://api.urbandictionary.com/v0/define?term=' + word
+    word_resp = requests.get(url=word_url)
+    translated_word = json.loads(word_resp.content)
+    found_word = translated_word['list'][0]['word']
+    definition = translated_word['list'][0]['definition']
+    example = translated_word['list'][0]['example']
+    intended_output = "Definition for {}: {}. Example: {}".format(found_word,definition,example)
+    
+    return intended_output
