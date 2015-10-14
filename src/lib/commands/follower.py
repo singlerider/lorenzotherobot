911025ac37f62ca56d14c1c92f3e3e5a23398260
Shane Engelman <haroboy876@gmail.com>
2015-10-14 18:05:18 +0000

    fix syntax error in follower.py

--- src/lib/commands/follower.py
+++ src/lib/commands/follower.py
@@ -3,6 +3,6 @@ from src.lib.twitch import *
 def follower(args):
     user = args[0]
     try:
-        return def get_follower_status(user)
+        return get_follower_status(user)
     except:
         return "It doesn't look like your Pokemon can evolve now. Sorry."
