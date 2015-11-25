3ad14d279fbb7e3892c0c32298370d58be8e0d34
Shane <haroboy876@gmail.com>
2015-11-25 10:36:34 -0800

    fix error message for non English chars in stream status

--- src/lib/commands/llama.py
+++ src/lib/commands/llama.py
@@ -48,7 +48,7 @@ def llama(args):
         get_offline_status_resp = requests.get(url=get_offline_status_url)
         offline_data = json.loads(get_offline_status_resp.content)
         try:
-            return str(offline_data["status"]) + " | " + str(offline_data["display_name"]) + " playing " + str(offline_data["game"])
+            return str(offline_data["status"].join(i for i in message if ord(i) < 128)) + " | " + str(offline_data["display_name"]) + " playing " + str(offline_data["game"])
         except:
             return "Dude. Either some weird HTTP request error happened, or the letters in the description are in Korean. Kappa"
 
