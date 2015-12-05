631f79656a44c73663eef13e0630963cc923f3af
Shane Engelman <haroboy876@gmail.com>
2015-12-05 19:07:12 +0000

    update schema to reflect 0 value for new entries points

--- schema.sql
+++ schema.sql
@@ -32,7 +32,7 @@ CREATE TABLE `custom_commands` (
   `response` varchar(200) NOT NULL,
   `times_used` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,7 +78,7 @@ CREATE TABLE `messages` (
   `message` varchar(2000) NOT NULL,
   `time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33401 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=42246 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +209,7 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `username` varchar(50) NOT NULL DEFAULT '',
-  `donation_points` int(11) DEFAULT NULL,
+  `donation_points` int(11) DEFAULT '0',
   `lastbattle` datetime DEFAULT NULL,
   `wins` int(11) DEFAULT '0',
   `losses` int(11) DEFAULT '0',
@@ -229,4 +229,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-04  1:33:07
+-- Dump completed on 2015-12-05 19:06:32
