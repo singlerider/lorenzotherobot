e13a1684f29680c61df8365fceab89712b1a819f
Shane <haroboy876@gmail.com>
2015-12-05 17:59:04 -0800

    add experimental random treats response phrases

--- src/lib/queries/points_queries.py
+++ src/lib/queries/points_queries.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from src.lib.queries.connection import *
-
+import random
 
 def mysql_ping():
     con = get_connection()
@@ -65,12 +65,25 @@ def get_all_user_points(username):  # gets all of a single user's points
         cur.execute("select donation_points, time_points from users where username = %s", [username])
         try:
             points = cur.fetchone()
-            donation_points = points
-            time_points = points
+            donation_points = points[0]
+            time_points = points[1]
+            total = donation_points + time_points
+            llama_responses = [
+                "All {0} your treats are belong to me".format(total),
+                "{0} treats, lit-tle man, put that shit in my hand".format(
+                    total),
+                "{0} - Mo' treats, mo' problems".format(total),
+                "1 treat, 2 treat, 3 treat, {0}".format(total),
+                "How many treats does it take to get to the center of a Tootsie Roll pop? {0}".format(
+                    total),
+                "{0} - the answer to the meaning of life.".format(total),
+                "{0} - treats game on fleek".format(total),
+                "{0} - very impressi - ResidentSleeper".format(total)
+                ]
+
             print cur.fetchone()#, donation_points, time_points
             if time_points > 0 or donation_points > 0:
-                return "All {0} your treats are belong to me".format(
-                    donation_points[0] + time_points[1])
+                return random.choice(llama_responses)
             else:
                 return "No treats found, but don't worry. You can earn them by watching the stream when it's live!"
         except Exception as error:
