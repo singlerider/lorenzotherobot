b0937c33d36f8c87e0d3184d65b5c86c21e92963
Shane <haroboy876@gmail.com>
2015-12-18 22:32:50 -0800

    update release command to allow '@' symbol

--- src/lib/commands/release.py
+++ src/lib/commands/release.py
@@ -3,8 +3,8 @@ from src.lib.queries.pokemon_queries import *
 
 def release(args):
     usage = '!release [position_number] [your_username]'
-    if args[1].lower() == globals.CURRENT_USER.lstrip("@"):
+    if args[1].lower().lstrip("@") == globals.CURRENT_USER:
         position = args[0]
-        return remove_user_pokemon(globals.CURRENT_USER, position)
+        return remove_user_pokemon(globals.CURRENT_USER.lstrip("@"), position)
     else:
         return usage
