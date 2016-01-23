19d5c9cb369c298a143b30528a4bc9da05bf887e
shane <haroboy876@gmail.com>
2016-01-22 22:41:17 -0800

    update release fix import

--- src/lib/commands/release.py
+++ src/lib/commands/release.py
@@ -1,10 +1,10 @@
+import globals
 from src.lib.queries.pokemon_queries import *
 
 
 def release(args):
-    usage = '!release [position_number] [your_username]'
     if args[1].lower().lstrip("@") == globals.CURRENT_USER:
         position = args[0]
         return remove_user_pokemon(globals.CURRENT_USER.lstrip("@"), position)
     else:
-        return usage
+        raise ValueError
