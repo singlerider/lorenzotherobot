715f7bd6e1e86f100ce72050fb3d0e9ca828cd7e
shane <haroboy876@gmail.com>
2016-01-23 22:32:47 -0800

    add initial osx setup file

---
+++ osx_setup.sh
@@ -0,0 +1,8 @@
+brew install mysql
+mysql.server start
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+mysql -uroot -e "GRANT ALL PRIVILEGES ON * . * TO 'root'@'localhost';"
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS twitch"
+mysql -uroot twitch < schema.sql
