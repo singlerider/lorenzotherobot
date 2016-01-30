6899f7497705a247ab25b6b07e76640904f2868a
shane <haroboy876@gmail.com>
2016-01-29 20:46:26 -0800

    update report command to github issues include github creds in globals

--- globals_example.py
+++ globals_example.py
@@ -1,4 +1,4 @@
-email_credentials = ["toaddr", "fromaddr", "fromaddrlogin"]
+GITHUB_AUTH = ["github-username", "bot-repo-name", "jnfiusefes9fe8jg9ewm8jg8emgwem9g9mjewg"]  # https://github.com/settings/tokens
 mysql_credentials = ['localhost', 'root', '', 'twitch']
 weather_key = ""  # from http://home.openweathermap.org/myservices
 CURRENT_CHANNEL = ""
