25399aa8981d6d8edd41d91fbc9d6a55f6ecdea4
shane <haroboy876@gmail.com>
2016-02-10 21:24:44 -0800

    consolidate treats columns

--- src/lib/commands/tallgrass.py
+++ src/lib/commands/tallgrass.py
@@ -21,16 +21,14 @@ def tallgrass(args):
     points_to_sacrifice = abs(int(args[0])) * -1
     username = globals.CURRENT_USER
     weighted_choices = [(0, 1), (1, 20), (2, 50), (3, 100)]
-    donation_points = get_user_points(username)
-    time_points = get_user_time_points(username)
-    if type(donation_points) != str:
-        actual_points = donation_points + time_points
+    points = get_user_points(username)
+    if type(points) != str:
         treats_removed = " " + str(points_to_sacrifice) + " treats from " + str(username) + "!"
     else:
-        actual_points = ""
-    if isinstance(actual_points, long):
+        points = ""
+    if isinstance(points, long):
         if abs(points_to_sacrifice) >= 1000:
-            if actual_points >= abs(points_to_sacrifice):
+            if points >= abs(points_to_sacrifice):
                 generated_pokemon = spawn_tallgrass(0)
                 if user_is_moderator(username) is False:
                     modify_user_points(username, points_to_sacrifice)
@@ -39,7 +37,7 @@ def tallgrass(args):
                 return "Sorry, but you need more treats to do that."
         elif abs(points_to_sacrifice) >= 100:
             if abs(points_to_sacrifice) <= 500:
-                if actual_points >= abs(points_to_sacrifice):
+                if points >= abs(points_to_sacrifice):
                     generated_pokemon = spawn_tallgrass(1)
                     if user_is_moderator(username) is False:
                         modify_user_points(username, points_to_sacrifice)
@@ -52,7 +50,7 @@ def tallgrass(args):
         elif abs(points_to_sacrifice) < 100:
             if abs(points_to_sacrifice) >= 25:
                 "abs(points_to_sacrifice) >= 25:", abs(points_to_sacrifice)
-                if actual_points >= abs(points_to_sacrifice):
+                if points >= abs(points_to_sacrifice):
                     generated_pokemon = spawn_tallgrass(2)
                     if user_is_moderator(username) is False:
                         modify_user_points(username, points_to_sacrifice)
@@ -62,7 +60,7 @@ def tallgrass(args):
                     return "Sorry, but you need more treats to do that."
             elif abs(points_to_sacrifice) > 4:
                 "abs(points_to_sacrifice) > 4:", abs(points_to_sacrifice)
-                if actual_points >= abs(points_to_sacrifice):
+                if points >= abs(points_to_sacrifice):
                     generated_pokemon = spawn_tallgrass(3)
                     if user_is_moderator(username) is False:
                         modify_user_points(username, points_to_sacrifice)
