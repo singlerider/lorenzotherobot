3b8f6ad6cd249c1e756d6020ca7225a1988fe8f9
shane <haroboy876@gmail.com>
2016-03-09 21:06:17 -0800

    fix follower command bug

--- src/lib/commands/follower.py
+++ src/lib/commands/follower.py
@@ -2,8 +2,9 @@ from src.lib.twitch import *
 
 
 def follower(args, **kwargs):
-    user = args[0]
+    username = args[0]
+    channel = kwargs.get("channel", "testchannel")
     try:
-        return get_follower_status(user)
+        return get_follower_status(username=username, channel=channel)
     except:
         return "It doesn't look like your Pokemon can evolve now. Sorry."
