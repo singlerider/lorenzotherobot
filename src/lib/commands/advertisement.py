af19bb983b945c12d0c29da863a7b4119be767f2
shane <haroboy876@gmail.com>
2016-05-29 21:37:43 -0700

    update advertisement

--- src/lib/commands/advertisement.py
+++ src/lib/commands/advertisement.py
@@ -6,12 +6,12 @@ def cron2(a=None):  # pragma: no cover
     return updates()
 
 
-def advertisement(**kwargs):  # pragma: no cover
+def advertisement(a=None):  # pragma: no cover
     return "If you like the designs and layouts, check out Nerdettedesigns's work at http://nerdettedesigns.com/"
 
 
 def updates():  # pragma: no cover
     return "New features and updates for me will be available in mid May. Next up: all bugs fixed."
 
-def custom_sounds(**kwargs):
+def custom_sounds(a=None):
     return "Donate to trigger some sounds! http://bit.ly/1Tv1L0J | http://bit.ly/24vwPst"
